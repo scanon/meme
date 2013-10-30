3b48dedbf2bd149f34182c8320d0a0a6c9a7ead4
Alexey Kazakov <aekazakov@lbl.gov>
2013-10-29 22:37:59 -0500

    minor update

---
+++ script-tests/test_meme_server_tug.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+
+# Script for testing back-end of meme service
+
+use strict;
+use warnings;
+
+use Bio::KBase::AuthToken;
+use Bio::KBase::AuthUser;
+
+my $user = "aktest";
+my $pw = "1475rokegi";
+
+my $auth_user = Bio::KBase::AuthUser->new();
+my $token = Bio::KBase::AuthToken->new( user_id => $user, password => $pw);
+$auth_user->get( token => $token->token );
+
+if ($token->error_message){
+	print $token->error_message."\n\n";
+	exit(1);
+};
+
+my $auth_token = $token->token;
+
+my $java_bin  = "../bin";
+my $java_lib = "../lib";
+
+my $command_line = "java -classpath $java_bin:$java_lib/* us.kbase.meme.MemeServerTug";
+
+my $ws = "AKtest";
+my $sequence_set_id = "KBase.SequenceSet.12345";
+my $meme_run_result_id = "\"kb|memerunresult.11\"";
+my $meme_pspm_collection_id = "\"kb|memepspmcollection.2\"";
+my $meme_pspm_id = "\"kb|memepspm.14\"";
+
+my $test_command = "";
+
+
+#1 help
+$test_command = $command_line." --help";
+print $test_command."\n\n";
+system ($test_command);
+
+#2 find_motifs_with_meme_from_ws
+$test_command = $command_line." --method find_motifs_with_meme_from_ws --ws $ws --query $sequence_set_id --mod oops --nmotifs 2 --minw 14 --maxw 28 --pal --token \"$auth_token\"";
+print $test_command."\n\n";
+system ($test_command);
+
+#3 get_pspm_collection_from_meme_result_from_ws
+$test_command = $command_line." --method get_pspm_collection_from_meme_result_from_ws --ws $ws --query $meme_run_result_id --token \"$auth_token\"";
+print $test_command."\n\n";
+system ($test_command);
+
+#4 compare_motifs_with_tomtom_by_collection_from_ws
+$test_command = $command_line." --method compare_motifs_with_tomtom_by_collection_from_ws --ws $ws --query $meme_pspm_collection_id --target $meme_pspm_collection_id --thresh 0.000001 --evalue --dist pearson --min_overlap 12 --token \"$auth_token\"";
+print $test_command."\n\n";
+system ($test_command);
+
+#5 compare_motifs_with_tomtom_from_ws
+$test_command = $command_line." --method compare_motifs_with_tomtom_from_ws --ws $ws --query $meme_pspm_id --target $meme_pspm_collection_id --thresh 0.000001 --evalue --dist pearson --min_overlap 12 --token \"$auth_token\"";
+print $test_command."\n\n";
+system ($test_command);
+
+#6 find_sites_with_mast_from_ws
+$test_command = $command_line." --method find_sites_with_mast_from_ws --ws $ws --query $meme_pspm_id --target $sequence_set_id --thresh 0.001 --token \"$auth_token\"";
+print $test_command."\n\n";
+system ($test_command);
+
+#7 find_sites_with_mast_by_collection_from_ws
+$test_command = $command_line." --method find_sites_with_mast_by_collection_from_ws --ws $ws --query $meme_pspm_collection_id --target $sequence_set_id --thresh 0.001 --token \"$auth_token\"";
+print $test_command."\n\n";
+system ($test_command);
+
+exit(0);
